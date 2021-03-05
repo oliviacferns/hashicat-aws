604941b453faef3e4d4fc41cd432046a99e8fc02
Olivia Fernandes <oliviacferns@gmail.com>
2021-03-05 21:00:31 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -115,12 +115,13 @@ resource "aws_eip_association" "hashicat" {
   allocation_id = aws_eip.hashicat.id
 }
 
-resource "aws_instance" "devops" {
+resource "aws_instance" "hashicat" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashicat.key_name
   associate_public_ip_address = true
   Billable                    = true
+  Department                  = devops
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
